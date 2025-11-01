5051b6f871a0a17e845635a524d471fffefa698b
Rajibul Islam <rajibul2009@gmail.com>
2025-11-01 12:01:14 +0000

    net text adding

--- run_mern_stack_with_docker_compose/frontend/src/components/Table.jsx
+++ run_mern_stack_with_docker_compose/frontend/src/components/Table.jsx
@@ -58,7 +58,7 @@ function Table() {
       <Toaster richColors closeButton />
       <div className="container mt-5">
         <h1 className="mb-4" id="h1">
-         User Table Latest webhook 
+         User Table Latest webhook for setting
         </h1>
         <CreateUserModal addUser={addUser} />
         <EditUserModal />
