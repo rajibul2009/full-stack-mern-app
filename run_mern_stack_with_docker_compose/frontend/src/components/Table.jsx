1ccc66b0073090f07716d8568645c577e025934f
rajibul2009 <rajibul2009@gmail.com>
2025-11-02 11:17:08 +0600

    table name edit

--- run_mern_stack_with_docker_compose/frontend/src/components/Table.jsx
+++ run_mern_stack_with_docker_compose/frontend/src/components/Table.jsx
@@ -58,7 +58,7 @@ function Table() {
       <Toaster richColors closeButton />
       <div className="container mt-5">
         <h1 className="mb-4" id="h1">
-         User Table feedback 
+         User Table log
         </h1>
         <CreateUserModal addUser={addUser} />
         <EditUserModal />
