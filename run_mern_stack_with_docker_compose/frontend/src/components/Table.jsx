34df7f2d6f8ebffef5e7588926ec64c06306533c
rajibul2009 <rajibul2009@gmail.com>
2025-11-02 14:34:31 +0600

    toke miscmached ok ok

--- run_mern_stack_with_docker_compose/frontend/src/components/Table.jsx
+++ run_mern_stack_with_docker_compose/frontend/src/components/Table.jsx
@@ -58,7 +58,7 @@ function Table() {
       <Toaster richColors closeButton />
       <div className="container mt-5">
         <h1 className="mb-4" id="h1">
-         User Table Webook toke miscmached ok
+         User Table Webook toke miscmached ok ok
         </h1>
         <CreateUserModal addUser={addUser} />
         <EditUserModal />
